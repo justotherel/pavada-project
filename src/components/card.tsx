98191318bdf504fe33393b0cc7ae5de398246e5f
igor <igorsmgl@gmail.com>
2022-05-04 22:20:02 +0300

    changed styles

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -6,19 +6,21 @@ interface CardProps {
 
 export default function Card(props: CardProps) {
   const card = (
-    <div className=" border-auxilary border-4 rounded-lg shadow-md shawod-black aspect-[1/1.5] hover:transition-transform hover:scale-110 min-h-[150px]">
+    <div className=" border-auxilary border-4 rounded-lg shadow-md shawod-black aspect-[1/1.5] hover:transition-transform hover:scale-110 min-h-[150px] md:min-h-[100px]">
       <div className="bg-rayan-gosling bg-no-repeat bg-cover shadow-xl relative h-full w-full">
         <div className="absolute left-1 text-center">
           <p className="text-red-700 font-extrabold text-2xl">{props.health}</p>
-          <p className="text-white font-extrabold text-2xl mt-[-10px]">{props.armor}</p>
+          <p className="text-white font-extrabold text-2xl mt-[-10px]">
+            {props.armor}
+          </p>
         </div>
       </div>
     </div>
   );
 
   const placeholder = (
-    <div className="border-4 border-auxilary border-dashed rounded-lg min-h-[20px] h-full aspect-[1/1.5]"></div>
+    <div className="border-4 border-auxilary border-dashed rounded-lg min-h-[150px] md:min-h-[100px] h-full aspect-[1/1.5]"></div>
   );
 
-  return <>{props.name !== 'placeholder' ? card : placeholder}</>;
+  return <>{props.name !== "placeholder" ? card : placeholder}</>;
 }
